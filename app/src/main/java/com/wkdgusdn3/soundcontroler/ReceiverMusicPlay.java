97d8ed7aaf46dcf871e040d9e322ff76cc27dfd5
wkdgusdn3 <wkdgusdn3@gmail.com>
2015-05-30 17:12:29 +0900

    음악 실행 기능 추가

---
+++ app/src/main/java/com/wkdgusdn3/soundcontroler/ReceiverMusicPlay.java
@@ -0,0 +1,25 @@
+package com.wkdgusdn3.soundcontroler;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.view.KeyEvent;
+
+public class ReceiverMusicPlay extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            Intent new_intent = new Intent(Intent.ACTION_MEDIA_BUTTON);
+            new_intent.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_HEADSETHOOK));
+            context.sendOrderedBroadcast(new_intent, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        new Thread(new Runnable() {
+//            public void run() {
+//                new Instrumentation().sendKeySync(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_HEADSETHOOK));
+//            }
+//        }).start();
+//    }
+    }
+}
--- app/src/main/java/com/wkdgusdn3/soundcontroler/SoundService.java
+++ app/src/main/java/com/wkdgusdn3/soundcontroler/SoundService.java
@@ -25,19 +25,23 @@ public class SoundService extends Service {
         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notification = new Notification(R.drawable.sound_icon, "SoundControler", System.currentTimeMillis());
 
+        Intent intent_play = new Intent(getApplicationContext(), ReceiverMusicPlay.class);
         Intent intent_up = new Intent(getApplicationContext(), ReceiverSoundUp.class);
         Intent intent_down = new Intent(getApplicationContext(), ReceiverSoundDown.class);
         Intent intent_mute = new Intent(getApplicationContext(), ReceiverSoundMute.class);
 
+        PendingIntent pendingIntent_play = PendingIntent.getBroadcast(context, 0, intent_play, 0);
         PendingIntent pendingIntent_up = PendingIntent.getBroadcast(context, 0, intent_up, 0);
         PendingIntent pendingIntent_down = PendingIntent.getBroadcast(context, 0, intent_down, 0);
         PendingIntent pendingIntent_mute = PendingIntent.getBroadcast(context, 0, intent_mute, 0);
 
         final RemoteViews views = new RemoteViews(this.getPackageName(), R.layout.sound_notification);
         views.setImageViewResource(R.id.sound_icon, R.drawable.sound_icon);
+        views.setImageViewResource(R.id.music_play, R.drawable.music_play);
         views.setImageViewResource(R.id.sound_up, R.drawable.sound_up);
         views.setImageViewResource(R.id.sound_down, R.drawable.sound_down);
         views.setImageViewResource(R.id.sound_mute, R.drawable.sound_mute);
+        views.setOnClickPendingIntent(R.id.music_play, pendingIntent_play);
         views.setOnClickPendingIntent(R.id.sound_up, pendingIntent_up);
         views.setOnClickPendingIntent(R.id.sound_down, pendingIntent_down);
         views.setOnClickPendingIntent(R.id.sound_mute, pendingIntent_mute);
