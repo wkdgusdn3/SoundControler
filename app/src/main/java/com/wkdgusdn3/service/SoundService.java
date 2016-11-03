b6b933fc63441734b9a736540f82ed0399420dee
wkdgusdn3 <wkdgusdn3@gmail.com>
2016-11-04 01:34:31 +0900

    불필요 코드 삭제

--- app/src/main/java/com/wkdgusdn3/service/SoundService.java
+++ app/src/main/java/com/wkdgusdn3/service/SoundService.java
@@ -69,51 +69,6 @@ public class SoundService extends Service {
             }
         }
 
-//        if (InfoManager.boolean_musicPlay) {
-//            Intent intent_play = new Intent(getApplicationContext(), ReceiverMusicPlay.class);
-//            PendingIntent pendingIntent_play = PendingIntent.getBroadcast(context, 0, intent_play, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.music_play, com.wkdgusdn3.soundcontroller.R.drawable.music_play);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.music_play, pendingIntent_play);
-//        } else {
-//            Intent intent_clear = new Intent(getApplicationContext(), ReceiverClear.class);
-//            PendingIntent pendingIntent_clear = PendingIntent.getBroadcast(context, 0, intent_clear, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.music_play, com.wkdgusdn3.soundcontroller.R.drawable.clear);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.music_play, pendingIntent_clear);
-//        }
-//        if (InfoManager.boolean_soundDown) {
-//            Intent intent_down = new Intent(getApplicationContext(), ReceiverSoundDown.class);
-//            PendingIntent pendingIntent_down = PendingIntent.getBroadcast(context, 0, intent_down, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_down, com.wkdgusdn3.soundcontroller.R.drawable.sound_down);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_down, pendingIntent_down);
-//        } else {
-//            Intent intent_clear = new Intent(getApplicationContext(), ReceiverClear.class);
-//            PendingIntent pendingIntent_clear = PendingIntent.getBroadcast(context, 0, intent_clear, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_down, com.wkdgusdn3.soundcontroller.R.drawable.clear);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_down, pendingIntent_clear);
-//        }
-//        if (InfoManager.boolean_soundUp) {
-//            Intent intent_up = new Intent(getApplicationContext(), ReceiverSoundUp.class);
-//            PendingIntent pendingIntent_up = PendingIntent.getBroadcast(context, 0, intent_up, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_up, com.wkdgusdn3.soundcontroller.R.drawable.sound_up);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_up, pendingIntent_up);
-//        } else {
-//            Intent intent_clear = new Intent(getApplicationContext(), ReceiverClear.class);
-//            PendingIntent pendingIntent_clear = PendingIntent.getBroadcast(context, 0, intent_clear, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_up, com.wkdgusdn3.soundcontroller.R.drawable.clear);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_up, pendingIntent_clear);
-//        }
-//        if (InfoManager.boolean_soundMute) {
-//            Intent intent_mute = new Intent(getApplicationContext(), ReceiverSoundMute.class);
-//            PendingIntent pendingIntent_mute = PendingIntent.getBroadcast(context, 0, intent_mute, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_mute, com.wkdgusdn3.soundcontroller.R.drawable.sound_mute);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_mute, pendingIntent_mute);
-//        } else {
-//            Intent intent_clear = new Intent(getApplicationContext(), ReceiverClear.class);
-//            PendingIntent pendingIntent_clear = PendingIntent.getBroadcast(context, 0, intent_clear, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_mute, com.wkdgusdn3.soundcontroller.R.drawable.clear);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_mute, pendingIntent_clear);
-//        }
-
         notification.contentView = views;
         notificationManager.notify(3, notification);
     }
@@ -154,51 +109,6 @@ public class SoundService extends Service {
                     break;
             }
         }
-
-//        if (InfoManager.boolean_musicPlay) {
-//            Intent intent_play = new Intent(getApplicationContext(), ReceiverMusicPlay.class);
-//            PendingIntent pendingIntent_play = PendingIntent.getBroadcast(context, 0, intent_play, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.music_play, com.wkdgusdn3.soundcontroller.R.drawable.music_play);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.music_play, pendingIntent_play);
-//        } else {
-//            Intent intent_clear = new Intent(getApplicationContext(), ReceiverClear.class);
-//            PendingIntent pendingIntent_clear = PendingIntent.getBroadcast(context, 0, intent_clear, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.music_play, com.wkdgusdn3.soundcontroller.R.drawable.clear);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.music_play, pendingIntent_clear);
-//        }
-//        if (InfoManager.boolean_soundDown) {
-//            Intent intent_down = new Intent(getApplicationContext(), ReceiverSoundDown.class);
-//            PendingIntent pendingIntent_down = PendingIntent.getBroadcast(context, 0, intent_down, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_down, com.wkdgusdn3.soundcontroller.R.drawable.sound_down);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_down, pendingIntent_down);
-//        } else {
-//            Intent intent_clear = new Intent(getApplicationContext(), ReceiverClear.class);
-//            PendingIntent pendingIntent_clear = PendingIntent.getBroadcast(context, 0, intent_clear, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_down, com.wkdgusdn3.soundcontroller.R.drawable.clear);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_down, pendingIntent_clear);
-//        }
-//        if (InfoManager.boolean_soundUp) {
-//            Intent intent_up = new Intent(getApplicationContext(), ReceiverSoundUp.class);
-//            PendingIntent pendingIntent_up = PendingIntent.getBroadcast(context, 0, intent_up, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_up, com.wkdgusdn3.soundcontroller.R.drawable.sound_up);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_up, pendingIntent_up);
-//        } else {
-//            Intent intent_clear = new Intent(getApplicationContext(), ReceiverClear.class);
-//            PendingIntent pendingIntent_clear = PendingIntent.getBroadcast(context, 0, intent_clear, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_up, com.wkdgusdn3.soundcontroller.R.drawable.clear);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_up, pendingIntent_clear);
-//        }
-//        if (InfoManager.boolean_soundMute) {
-//            Intent intent_mute = new Intent(getApplicationContext(), ReceiverSoundMute.class);
-//            PendingIntent pendingIntent_mute = PendingIntent.getBroadcast(context, 0, intent_mute, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_mute, com.wkdgusdn3.soundcontroller.R.drawable.sound_mute);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_mute, pendingIntent_mute);
-//        } else {
-//            Intent intent_clear = new Intent(getApplicationContext(), ReceiverClear.class);
-//            PendingIntent pendingIntent_clear = PendingIntent.getBroadcast(context, 0, intent_clear, 0);
-//            views.setImageViewResource(com.wkdgusdn3.soundcontroller.R.id.sound_mute, com.wkdgusdn3.soundcontroller.R.drawable.clear);
-//            views.setOnClickPendingIntent(com.wkdgusdn3.soundcontroller.R.id.sound_mute, pendingIntent_clear);
-//        }
 
         notification.contentView = views;
         notificationManager.notify(3, notification);
